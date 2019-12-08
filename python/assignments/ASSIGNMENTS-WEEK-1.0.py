8112b297ebb03ab28b5e5e178bc0ffbc8835d179
Topika182 <donjardany@gmail.com>
2019-12-08 13:48:54 -0500

    Modified string format method

--- python/assignments/ASSIGNMENTS-WEEK-1.0.py
+++ python/assignments/ASSIGNMENTS-WEEK-1.0.py
@@ -4,6 +4,6 @@
 num = int(input('Enter a number: '))
 # Checks if the number input is even or odd.(Using a if else statement)
 if (num % 2) == 0:
-    print('Even'.format(num))
+    print(f'{num} is Even')
 else:
-    print('Odd'.format(num))
+    print(f'{num} is Odd')
